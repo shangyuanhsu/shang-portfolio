048f2d260ec13b58bb2b103b1d5856fd710411a8
shang <hsushangyuan@gmail.com>
2023-05-19 18:31:20 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en", "tw"],
+    defaultLocale: "tw",
+    localeDetection: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
