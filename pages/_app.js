85d143a2343f89bc39dd9400855eacf60ba428a8
shang <hsushangyuan@gmail.com>
2023-05-22 20:49:23 +0800

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import '../styles/globals.css'
 import Layuot from '../components/Layout';
 import ThemeContextProvider from "../contexts/ThemeContext"
+import Script from 'next/script';
 
 function App({ Component, pageProps }) {
   return (
     <ThemeContextProvider>
-    <Layuot>
-      <Component {...pageProps} />
-    </Layuot>
+      <Layuot>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R73JK0G1BX" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {
+            `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-R73JK0G1BX');
+          `
+          }
+
+        </Script>
+        <Component {...pageProps} />
+      </Layuot>
     </ThemeContextProvider>
   );
 }
